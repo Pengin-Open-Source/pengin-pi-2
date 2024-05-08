256a3dc8b0e521c997324f8e4565e08c003721f7
mvetom <onelchela@live.com>
2024-05-08 15:58:06 -0400

    add markdownit back,  add whitenoise

--- app/settings.py
+++ app/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'messenger',
     'webmaster',
     'macros',
+    'markdownit',
 
 ]
 
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'public.urls'
