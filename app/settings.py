30e2285cd3876d5a11239b832ce15a693d7edb08
mvetom <onelchela@live.com>
2024-05-09 09:24:43 -0400

    add staticfiles_dirs to get static files to load

--- app/settings.py
+++ app/settings.py
@@ -138,6 +138,11 @@ USE_TZ = True
 STATIC_URL = 'app/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'app/static',
+]
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
