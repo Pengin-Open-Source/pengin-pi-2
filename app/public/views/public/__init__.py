9c1a7e01adfd5e8c8e8f14baa67bb9d0b372cd8c
stuart909 <stuart@tobupengin.com>
2024-05-30 15:02:49 -0500

    added paginate utility in app/util/paginate.py

--- app/public/views/public/__init__.py
+++ app/public/views/public/__init__.py
@@ -11,13 +11,25 @@ def about(request):
     return render(request, 'about.html')
 
 # Define the blog view
-def blog(request):
-    return render(request, 'blog.html')
+def blogs(request):
+    return render(request, 'blogs.html')
+
+# Define the blog post view
+def post(request, id):
+    return render(request, 'post.html')
 
 # Define the products view
 def products(request):
     return render(request, 'products.html')
 
+# Define the product view
+def product(request, id):
+    return render(request, 'product.html')
+
 # Define the jobs view
 def jobs(request):
     return render(request, 'jobs.html')
+
+# Define the job view
+def job(request, id):
+    return render(request, 'job.html')
