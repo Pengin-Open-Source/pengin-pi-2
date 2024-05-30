37a925dd70e1f6c264f0e7daa35b58e229201790
stuart909 <stuart@tobupengin.com>
2024-05-30 17:21:43 -0500

    added products

--- app/public/views/public/__init__.py
+++ app/public/views/public/__init__.py
@@ -1,9 +1,10 @@
-from django.http import HttpResponse
-from models import BlogPost
+from django.http import HttpResponse, HttpRequest
+from models import BlogPost, Product
 from util import paginate  # Assuming you have a pagination utility
 # Define the blog post view
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import user_passes_test
+from app.util.s3 import conn
 
 
 # Define the home view
@@ -56,12 +57,36 @@ def post(request, post_id):
 
 
 # Define the products view
-def products(request):
-    return render(request, 'products.html')
+def products(request: HttpRequest):
+    if request.method == "POST":
+        page = int(request.POST.get("page_number", 1))
+    else:
+        page = 1
+
+    products = paginate(Product, page=page, key="name", pages=9)
+    is_admin = user_passes_test(lambda u: u.is_staff)(request.user)
+    for product in products:
+        product.card_image_url = conn.get_URL(product.card_image_url)
+
+    return render(request, "products/products.html", {
+        "is_admin": is_admin,
+        "products": products,
+        "page": page,
+        "primary_title": "Products",
+    })
 
 # Define the product view
-def product(request, id):
-    return render(request, 'product.html')
+def product(request: HttpRequest, product_id: int):
+    product = get_object_or_404(Product, id=product_id)
+    product.stock_image_url = conn.get_URL(product.stock_image_url)
+    is_admin = user_passes_test(lambda u: u.is_staff)(request.user)
+
+    return render(request, "products/product.html", {
+        "is_admin": is_admin,
+        "product": product,
+        "page": 1,
+        "primary_title": product.name,
+    })
 
 # Define the jobs view
 def jobs(request):
