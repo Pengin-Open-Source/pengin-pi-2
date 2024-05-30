49abe91e77168a01f07d0020aa0bd391c99daaf8
stuart909 <stuart@tobupengin.com>
2024-05-30 18:24:53 -0500

    added job, post, and product ids url

--- app/public/urls/__init__.py
+++ app/public/urls/__init__.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from public.views.public import home, about, blogs, post, products, jobs
+from public.views.public import home, about, blogs, post, products, jobs, product, job
 
 urlpatterns = [
     path('', home, name='home'),
@@ -26,12 +26,13 @@ urlpatterns = [
     path('about.html', about, name='about'),
     path('blogs', blogs, name='blogs'),
     path('blogs.html', blogs, name='blogs'),
-    path('post/<int:post_id>/', post, name='post'),
-    path('post.html/<int:post_id>/', post, name='post'),
+    path('blogs/<int:post_id>/', post, name='post'),
     path('products', products, name='products'),
     path('products.html', products, name='products'),
+    path('products/<int:product_id>/', product, name='product'),
     path('jobs', jobs, name='jobs'),
     path('jobs.html', jobs, name='jobs'),
+    path('jobs/<int:job_id>/', job, name='job'),
 ]
 
 
