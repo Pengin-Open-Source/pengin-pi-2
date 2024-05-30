beb2e5144f9b3076a59f250639638d3cd17623de
stuart909 <stuart@tobupengin.com>
2024-05-30 13:31:38 -0500

    added relationships

--- app/models/product/__init__.py
+++ app/models/product/__init__.py
@@ -1,6 +1,7 @@
 from django.db import models
 import uuid
 
+
 class Product(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4())
     name = models.CharField(max_length=100)
