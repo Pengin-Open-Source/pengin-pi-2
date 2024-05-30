bd363945cdbcc58d3d1c3ea9882d960d1a48c10b
stuart909 <stuart@tobupengin.com>
2024-05-30 12:46:39 -0500

    added application and calendar models

---
+++ app/models/application/__init__.py
@@ -0,0 +1,25 @@
+from django.db import models
+import uuid
+from datetime import datetime
+
+
+class StatusCode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=20, unique=True, nullable=False)
+
+    def __str__(self):
+        return self.code
+
+
+class Application(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    resume_path = models.CharField(max_length=100)
+    cover_letter_path = models.CharField(max_length=100)
+    message = models.TextField()
+    location = models.CharField(max_length=100)
+    date_applied = models.DateTimeField(null=True, blank=True)
+    status_code = models.ForeignKey(StatusCode, on_delete=models.CASCADE)
+    status_code_date_change = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Application {self.id} - {self.status_code}"
