a71b97c44b01cbef25d9b2ed3af95fb05d940347
mvetom <onelchela@live.com>
2024-06-04 10:30:11 -0400

    We can now get to the home page when we run this branch. app/app/urls.py sends calls to the root url to public's urls.py,  which in turn directs us to the home view. (home view and home.html added in). Adjusted settings.py slightly so Django can find the top level templates folder.
    Got rid of some files in models that were not needed.

--- app/app/settings.py
+++ app/app/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'app', 'templates'),],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
