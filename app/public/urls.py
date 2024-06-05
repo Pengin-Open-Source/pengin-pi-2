1c41b7f741509e78f303dd68099f0a235bd778b2
mvetom <onelchela@live.com>
2024-06-05 11:27:04 -0400

    New "empty" branch for another setup

--- app/public/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from public.views import home
-
-urlpatterns = [
-    path('', home, name='home'),
-    path('home', home, name='home'),
-    path('index', home, name='home'),
-    path('index.html', home, name='home'),
-]
--- app/public/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-
-# Define the home view
-def home(request):
-    return render(request, 'home.html')
