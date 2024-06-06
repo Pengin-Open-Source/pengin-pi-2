77f90e164acc488cc31aecede30d4a9336c413a3
mvetom <onelchela@live.com>
2024-06-06 10:34:43 -0400

    Move some models over from Michele-jun4 into jobs,  events,  and blogs

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+import uuid
+from datetime import datetime
 
-# Create your models here.
+
+class Job(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    job_title = models.CharField(max_length=100)
+    short_description = models.CharField(max_length=280)
+    long_description = models.TextField()
+    department = models.CharField(max_length=100)
+    salary = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    hiring_manager = models.CharField(max_length=100)
+    date_posted = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return str(self.job_title)
+
+
+class StatusCode(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    code = models.CharField(max_length=20, unique=True, nullable=False)
+
+    def __str__(self):
+        return str(self.code)
+
+
+class Application(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    resume_path = models.CharField(max_length=100)
+    cover_letter_path = models.CharField(max_length=100)
+    message = models.TextField()
+    location = models.CharField(max_length=100)
+    date_applied = models.DateTimeField(null=True, blank=True)
+    status_code = models.ForeignKey(StatusCode, on_delete=models.CASCADE)
+    status_code_date_change = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"Application {self.id} - {self.status_code}"
