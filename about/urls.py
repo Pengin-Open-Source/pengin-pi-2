18a33f17e24e6a2a32334d812ffb4dc2ea1cdb09
stuart909 <stuart@tobupengin.com>
2024-06-06 15:43:29 -0500

    fixed about views, templates, and configs for about

--- about/urls.py
+++ about/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from views import about
+from .views import about
 
 urlpatterns = [
     path('about', about, name='about'),
--- about/views.py
+++ about/views.py
@@ -1,16 +1,18 @@
-from models import About
+from .models import About
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
-from util.s3 import conn
+from util.s3 import File
 from botocore.exceptions import ParamValidationError
 from util.defaults import default
 import logging
 
 
+conn = File()
+
 # Define the about view
 def about(request):
     about = About.objects.first() or default.About()
-    is_admin = user_passes_test(lambda u: u.is_staff)(request.user)
+    is_admin = request.user.is_authenticated and request.user.is_staff
     try:
         image = conn.get_URL(about.image)
     except ParamValidationError:
