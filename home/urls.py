7d5877a51b6bafb4f235598a3bd4a7be9aac4b89
stuart909 <stuart@tobupengin.com>
2024-06-06 13:15:10 -0500

    finished home public route

--- home/urls.py
+++ home/urls.py
@@ -1,23 +1,13 @@
 from django.urls import path
-from public.views.public import home, about, blogs, post, products, jobs, product, job
+from views import home
+
 
 urlpatterns = [
     path('', home, name='home'),
     path('home', home, name='home'),
     path('index', home, name='home'),
     path('index.html', home, name='home'),
-    path('about', about, name='about'),
-    path('about.html', about, name='about'),
-    path('blogs', blogs, name='blogs'),
-    path('blogs.html', blogs, name='blogs'),
-    path('blogs/<int:post_id>/', post, name='post'),
-    path('products', products, name='products'),
-    path('products.html', products, name='products'),
-    path('products/<int:product_id>/', product, name='product'),
-    path('jobs', jobs, name='jobs'),
-    path('jobs.html', jobs, name='jobs'),
-    path('jobs/<int:job_id>/', job, name='job'),
-]
+    ]
 
 
 
