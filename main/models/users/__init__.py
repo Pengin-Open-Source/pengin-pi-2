56ce2262306dcbf2504507e2568b13f23190f6bb
stuart909 <stuart@tobupengin.com>
2024-06-06 11:31:25 -0500

    moved users model into main/models/users/__init__.py because users will likely be a primary dependency for much of the project and not compatible with being loaded by relationships at the end of the installed_apps list

--- main/models/users/__init__.py
+++ main/models/users/__init__.py
@@ -58,25 +58,5 @@ class UserRoles(models.Model):
         return str(self.id)
 
 
-class Company(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(max_length=50, unique=True)
-    phone = models.CharField(max_length=50)
-    city = models.CharField(max_length=50)
-    state = models.CharField(max_length=50)
-    country = models.CharField(max_length=50)
-    zipcode = models.CharField(max_length=50)
-    email = models.EmailField(max_length=100, unique=True)
-    address1 = models.CharField(max_length=50)
-    address2 = models.CharField(max_length=50)
 
-    def __str__(self):
-        return self.name
-
-
-class CompanyMembers(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-
-    def __str__(self):
-        return str(self.id)
 
