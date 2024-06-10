203b4ca521bbec15a3c8b48a79b7099d33553779
franfif <francis.barrow@gmail.com>
2024-06-10 16:13:19 -0400

    Update products urls with app_name

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path
 from .views import products, product, create_product
 
+# Add namespace to urls
+app_name = 'products'
+
 urlpatterns = [
-    path('products', products, name='products'),
-    path('products.html', products, name='products'),
-    path('products/<int:product_id>/', product, name='product'),
+    path('', products, name='products'),
+    path('<int:product_id>/', product, name='product'),
     path('create/', create_product, name='create_product'),
+    # path('edit/<int:product_id>/', edit_product, name='edit_product'),
+    # path('delete/<int:product_id>/', delete_product, name='delete_product'),
+    # path('<int:product_id>/add_image/', add_image, name='add_image'),
 ]
 
 
