4a9b5b90468647d3b8dae833bed4f014e6b411aa
franfif <francis.barrow@gmail.com>
2024-06-11 16:29:08 -0400

    Refactor view create_product to create_edit_product to handle both actions

--- products/views.py
+++ products/views.py
@@ -1,41 +1,75 @@
 from django.http import HttpRequest
 from .models import Product
 from django.core.paginator import Paginator
-# Define the blog post view
 from django.shortcuts import render, get_object_or_404, redirect
+from botocore.exceptions import ParamValidationError
 from util.s3 import File
 from util.security.auth_tools import is_admin_provider, is_admin_required
 from .forms import ProductForm
 from werkzeug.utils import secure_filename
-# views.py
 
 conn = File()
 
 
 @is_admin_required
-def create_product(request):
-    form = ProductForm()
+def create_edit_product(request, product_id=None):
+    context = {}
+    # Check if product_id is provided: if so, get and edit the product
+    if product_id:
+        product = get_object_or_404(Product, id=product_id)
+        try:
+            product.card_image_url = conn.get_URL(product.card_image_url)
+        except ParamValidationError:
+            product.card_image_url = None
+        try:
+            product.stock_image_url = conn.get_URL(product.stock_image_url)
+        except ParamValidationError:
+            product.stock_image_url = None
 
-    if request.method == "POST":
-        form = ProductForm(request.POST, request.FILES)
-        if form.is_valid():
-            product = form.save(commit=False)
+        context["card_image_url"] = product.card_image_url
+        context["stock_image_url"] = product.stock_image_url
 
-            large_file = request.FILES.get('file_large')
-            small_file = request.FILES.get('file_small')
+        # If the request method is POST, create a form with the request data and the product instance
+        if request.method == "POST":
+            form = ProductForm(request.POST, request.FILES, instance=product)
+        # If the request method is GET, create a form with the product instance
+        else:
+            form = ProductForm(instance=product)
+        context["primary_title"] = f'Edit Product: {product.name}'
+        context["action"] = "update"
 
-            if large_file:
-                large_file.filename = secure_filename(large_file.name)
-                product.stock_image_url = conn.create(large_file)
+    # Create a new product
+    else:
+        # If the request method is POST, create a form with the request data
+        if request.method == "POST":
+            form = ProductForm(request.POST, request.FILES)
+        # If the request method is GET, create a blank form
+        else:
+            form = ProductForm()
+        context["primary_title"] = "Create Product"
+        context["action"] = "create"
 
-            if small_file:
-                small_file.filename = secure_filename(small_file.name)
-                product.card_image_url = conn.create(small_file)
+    context["form"] = form
 
-            product.save()
-            return redirect('products:detail-product', product_id=product.id)
+    # If the request method is POST and the form is valid, save the product
+    if request.method == "POST" and form.is_valid():
+        product = form.save(commit=False)
 
-    return render(request, 'product_create.html', {'form': form, 'primary_title': "Create Product"})
+        large_file = request.FILES.get('file_large')
+        small_file = request.FILES.get('file_small')
+
+        if large_file:
+            large_file.filename = secure_filename(large_file.name)
+            product.stock_image_url = conn.create(large_file)
+
+        if small_file:
+            small_file.filename = secure_filename(small_file.name)
+            product.card_image_url = conn.create(small_file)
+
+        product.save()
+        return redirect('products:detail-product', product_id=product.id)
+
+    return render(request, "product_form.html", context)
 
 
 # Define the products view
