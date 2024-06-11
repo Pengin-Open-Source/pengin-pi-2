5d22e926b03d6a8947013a39a5b8f6785aa336f9
mvetom <onelchela@live.com>
2024-06-11 19:06:57 -0400

    Added some suggested UserAdmin code,  and registered UserAdmin along with Permission, to get past a Foreign Key Constraint error I get when I try to edit users in the admin site.  Still getting the error, so must do more tweaking

--- main/admin.py
+++ main/admin.py
@@ -1,6 +1,31 @@
 # admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import Permission
+
 
 from .models import User
 
-admin.site.register(User)
+
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('name',)}),
+        ('Permissions', {'fields': ('is_staff',
+         'is_active', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'password1', 'password2', 'is_staff', 'is_active')}
+         ),
+    )
+    search_fields = ('email', 'name')
+    ordering = ('email',)
+    filter_horizontal = ('groups', 'user_permissions')
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Permission)
