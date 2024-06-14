bea8f4029ce69a28447515efddb6a9a1dcdb1390
mvetom <onelchela@live.com>
2024-06-14 08:40:30 -0400

    Correct str function for BlogPost, clarify reference to blogs.views

--- blogs/models.py
+++ blogs/models.py
@@ -11,4 +11,4 @@ class BlogPost(models.Model):
     tags = models.CharField(max_length=150)
 
     def __str__(self):
-        return self.title
+        return str(self.title)
--- blogs/urls.py
+++ blogs/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path
-from .views import blogs, post
+from blogs.views import blogs, post
 
 urlpatterns = [
     path('blog', blogs, name='blogs'),
     path('blogs.html', blogs, name='blogs'),
     path('blogs/<int:post_id>/', post, name='post'),
 ]
-
-
-
