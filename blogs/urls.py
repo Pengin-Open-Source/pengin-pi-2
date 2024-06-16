7d0ed38832e6a38604445726fe532c82b251d277
mvetom <onelchela@live.com>
2024-06-16 10:36:59 -0400

    Add URL for create blog

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from blogs.views import blogs, post
+from blogs.views import blogs, post, create_post
 
 urlpatterns = [
     path('blog', blogs, name='blogs'),
     path('blogs.html', blogs, name='blogs'),
     path('blogs/<int:post_id>/', post, name='blog_post'),
+    path('blog/create/', create_post, name='create_blog'),
 ]
