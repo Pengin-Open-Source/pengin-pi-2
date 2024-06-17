1262394cd1cf175fb5f167a9f88b5bf91bcd3e62
franfif <francis.barrow@gmail.com>
2024-06-17 11:10:02 -0400

    Replace create and edit product url patterns

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import product_list, product_detail, create_edit_product, delete_product
+from .views import product_list, product_detail, create_product, edit_product, delete_product
 
 # Add namespace to urls
 app_name = 'products'
@@ -7,8 +7,8 @@ app_name = 'products'
 urlpatterns = [
     path('', product_list, name='list-products'),
     path('<uuid:product_id>/', product_detail, name='detail-product'),
-    path('create/', create_edit_product, name='create-product'),
-    path('<uuid:product_id>/edit/', create_edit_product, name='edit-product'),
+    path('create/', create_product, name='create-product'),
+    path('<uuid:product_id>/edit/', edit_product, name='edit-product'),
     path('<uuid:product_id>/delete/', delete_product, name='delete-product'),
     # path('<int:product_id>/add_image/', add_image, name='add_image'),
 ]
