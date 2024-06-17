1db6c32055155aa717971c05f0fb63fb9a0e9620
mvetom <onelchela@live.com>
2024-06-17 15:45:31 -0400

    Update urls/references for blogs.  Switch blog menu links to refer to { % url %}'s instead of relative links

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from blogs.views import blogs, post, create_post
 
+app_name = 'blogs'
+
 urlpatterns = [
     path('blog', blogs, name='blogs'),
+    path('blogs', blogs, name='blogs'),
     path('blogs.html', blogs, name='blogs'),
     path('blogs/<int:post_id>/', post, name='blog_post'),
-    path('blog/create/', create_post, name='create_blog_post'),
+    path('blogs/create/', create_post, name='create_blog_post'),
 ]
