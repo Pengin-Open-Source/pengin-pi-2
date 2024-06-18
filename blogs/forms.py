b9f674b6e268676cbda3b1db1f1546362211728e
mvetom <onelchela@live.com>
2024-06-18 10:22:32 -0400

    add in new fields to the BlogForm

--- blogs/forms.py
+++ blogs/forms.py
@@ -7,6 +7,7 @@ from blogs.models import BlogPost
 class BlogForm(forms.ModelForm):
     class Meta:
         model = BlogPost
-        fields = ['title', 'date',  'content', 'tags']
+        fields = ['title',  'author', 'edited_by',
+                  'date',  'content', 'tags',  'method']
         widgets = {'content': forms.Textarea(
             attrs={'cols': 120, 'rows': 40}), }
