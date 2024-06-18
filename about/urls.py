61456efca12759aa26728d48d33ed07ffd13839f
stuart909 <stuart@tobupengin.com>
2024-06-17 23:32:56 -0500

    added rough draft of about editor: forms, templates, views, urls, orm

--- about/urls.py
+++ about/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from .views import about
+from .views import about, edit_about, create_about
 
 urlpatterns = [
     path('about', about, name='about'),
     path('about.html', about, name='about'),
+    path('about/edit/', edit_about, name='edit_about'),
+    path('about/create/', create_about, name='create_about'),
 ]
-
-
-
--- about/views.py
+++ about/views.py
@@ -1,16 +1,60 @@
 from .models import About
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required, permission_required
+from django.core.exceptions import PermissionDenied
+from django.utils.decorators import method_decorator
 from util.s3 import File
 from botocore.exceptions import ParamValidationError
 from util.defaults import default
 import logging
-from util.security.auth_tools import is_admin_provider
+from util.security.auth_tools import is_admin_provider,is_admin_required
+from .forms import AboutForm
+
 
 
 conn = File()
+# views.py
+@method_decorator(login_required)
+@method_decorator(is_admin_required)
+def edit_about(request):
+    about = About.objects.first()
+    
+    if request.method == 'POST':
+        form = AboutForm(request.POST, request.FILES, instance=about)
+        if form.is_valid():
+            form.save()
+            return redirect('about_view')
+    else:
+        form = AboutForm(instance=about)
+    
+    context = {
+        'form': form,
+        'about': about,
+        'image': about.image.url if about and about.image else None,
+        'primary_title': "Edit About Page"
+    }
+    return render(request, 'about/edit.html', context)
+
+@method_decorator(login_required)
+@method_decorator(is_admin_required)
+def create_about(request):
+    if request.method == 'POST':
+        form = AboutForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('about_view')
+    else:
+        form = AboutForm()
+    
+    context = {
+        'form': form,
+        'primary_title': "Create About Page"
+    }
+    return render(request, 'about/create.html', context)
+
 
 # Define the about view
-@is_admin_provider
+@method_decorator(is_admin_provider)
 def about(request, is_admin):
     about = About.objects.first() or default.About()
     try:
