487c3438c176d807da4bd7bca8d269173a88e79f
stuart909 <stuart@tobupengin.com>
2024-06-18 00:00:29 -0500

    added rough cut of job editor: views, urls, templates, orm, forms

--- jobs/urls.py
+++ jobs/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import jobs, job
+from .views import jobs, job, create_job, edit_job, delete_job
 
 urlpatterns = [
     path('jobs', jobs, name='jobs'),
     path('jobs.html', jobs, name='jobs'),
     path('jobs/<int:job_id>/', job, name='job'),
+    path('jobs/create/', create_job, name='create_job'),
+    path('jobs/<uuid:job_id>/edit/', edit_job, name='edit_job'),
+    path('jobs/<uuid:job_id>/delete/', delete_job, name='delete_job'),
 ]
 
 
