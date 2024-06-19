667a58463650ded055d275c162863d52515f009b
mvetom <onelchela@live.com>
2024-06-19 10:11:04 -0400

    Add in ability to add fields to cleaned data after form validation - (for fields that were never part of the form,  'method' for blogs, which never needs to be seen or edited by the user, or for fields left out of a particular rendering of the form - like date,  which sometimes needs to be seen, but never edited, by the user )

--- blogs/forms.py
+++ blogs/forms.py
@@ -23,9 +23,20 @@ class BlogForm(forms.ModelForm):
             if field_name in self.fields:
                 self.initial[field_name] = value
 
+    # Set values for fields,  - useful when you want
+    # to add in new fields that weren't included in the form
+    # and wouldn't have gone through form validation.
+    # For example, in blogs, don't need the Method field to
+    # ever be on the template version of the form,  and I don't
+    # want users to be able to edit the date of the blog post.
+    def set_cleaned_data_field(self, field, value):
+        # if we ever need validation logic... can
+        # check here
+        self.cleaned_data[field] = value
+
     class Meta:
         model = BlogPost
         fields = ['title',  'author', 'edited_by',
-                  'date',  'content', 'tags',  'method']
+                  'date',  'content', 'tags']
         widgets = {'content': forms.Textarea(
             attrs={'cols': 120, 'rows': 40}), }
