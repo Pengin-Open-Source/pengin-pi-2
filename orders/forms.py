d76c5b471e79d4ccf3100d83fabcfeb701946188
franfif <francis.barrow@gmail.com>
2024-06-24 14:12:34 -0400

    Add clean method to customer form

--- orders/forms.py
+++ orders/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core.exceptions import ValidationError
 from .models import Order, Customer
 
 
@@ -10,6 +11,15 @@ class OrderForm(forms.ModelForm):
 
 
 class CustomerForm(forms.ModelForm):
+
+    def clean(self):
+        cleaned_data = super().clean()
+        user = cleaned_data.get('user')
+        company = cleaned_data.get('company')
+        if user and company:
+            raise ValidationError('Customer cannot have both user and company relationships.')
+        if not user and not company:
+            raise ValidationError('Customer must have either a user or a company relationship.')
 
     class Meta:
         model = Customer
