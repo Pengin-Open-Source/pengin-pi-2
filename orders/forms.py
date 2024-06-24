71b32de0ba872e3e305404c741ee7c9b1f46dd19
franfif <francis.barrow@gmail.com>
2024-06-24 10:57:29 -0400

    Add customer form

--- orders/forms.py
+++ orders/forms.py
@@ -6,4 +6,11 @@ class OrderForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = ['order_date', 'customer']
+        fields = ['order_date', 'customer',]
+
+
+class CustomerForm(forms.ModelForm):
+
+    class Meta:
+        model = Customer
+        fields = ['user', 'company',]
