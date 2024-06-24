d8dca1b16987f29c15d94719bbc2d3440d21d385
mvetom <onelchela@live.com>
2024-06-24 09:17:59 -0400

    Update url path to specific blogpost to use a uuid instead of and int id; can now see a list of blog titles with clickable links

--- blogs/urls.py
+++ blogs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('blog', blogs, name='blogs'),
     path('blogs', blogs, name='blogs'),
     path('blogs.html', blogs, name='blogs'),
-    path('blogs/<int:post_id>/', post, name='blog_post'),
+    path('blogs/<uuid:post_id>/', post, name='blog_post'),
     path('blogs/create/', create_post, name='create_blog_post'),
 ]
