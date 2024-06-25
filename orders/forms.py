9a9b61c1d3a84038b8f7a3ab381fa5bbb0a2a988
franfif <francis.barrow@gmail.com>
2024-06-25 15:29:07 -0400

    Create OrderProductForm and OrderProductFormSet

--- orders/forms.py
+++ orders/forms.py
@@ -1,13 +1,28 @@
 from django import forms
-from django.core.exceptions import ValidationError
-from .models import Order, Customer
+
+from .models import Order, Customer, OrderProduct
+
+
+class OrderProductForm(forms.ModelForm):
+    class Meta:
+        model = OrderProduct
+        fields = ['product', 'quantity']
+
+
+OrderProductFormSet = forms.inlineformset_factory(
+    Order,
+    OrderProduct,
+    form=OrderProductForm,
+    extra=1,
+    can_delete=True,
+)
 
 
 class OrderForm(forms.ModelForm):
 
     class Meta:
         model = Order
-        fields = ['order_date', 'customer',]
+        fields = ['order_date', 'customer', 'products',]
 
 
 class CustomerForm(forms.ModelForm):
