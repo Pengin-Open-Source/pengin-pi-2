4d299b7e444e8bfc8904e5bd1625a064ffa742d5
franfif <francis.barrow@gmail.com>
2024-07-01 14:38:31 -0400

    Remove unnecessary parenthesis in "unique together"

--- orders/models.py
+++ orders/models.py
@@ -73,7 +73,7 @@ class OrderProduct(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False)
 
     class Meta:
-        unique_together = (('order', 'product'),)
+        unique_together = ('order', 'product')
 
 
 # ### Order Workflow Models ###
