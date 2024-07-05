ad3df35a2dc924ed58b601df46195c2854ac4715
Sincere Banks <abanks7296@gmail.com>
2024-07-05 11:01:56 -0400

    standardize pagination variables, set foreignkey relationship null=true instead of default user

--- companies/models.py
+++ companies/models.py
@@ -22,7 +22,7 @@ class Company(models.Model):
 class CompanyMembers(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='members', null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=uuid.UUID('3b2cc328-41f9-4729-b301-b32f18a69578'))
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Allow null for user
 
 
     def __str__(self):
--- companies/views.py
+++ companies/views.py
@@ -6,26 +6,20 @@ from main.models.users import User
 from .forms import CompanyForm
 from django_ratelimit.decorators import ratelimit
 
-
 @login_required
 def display_companies_home(request):
     if request.user.has_perm('admin_permission'):
         return handle_admin_view(request)
     else:
         return handle_user_view(request)
-    
-
 
 def handle_admin_view(request):
-    if request.method == "POST":
-        page = int(request.POST.get('page_number', 1))
-    else:
-        page = 1
+    page_number = request.POST.get('page_number', 1) if request.method == "POST" else request.GET.get('page', 1)
     companies = Company.objects.all()
     paginator = Paginator(companies, 10)  # 10 companies per page
-    companies_page = paginator.get_page(page)
+    page_obj = paginator.get_page(page_number)
     return render(request, 'company_info_main.html', {
-        'companies': companies_page, 'is_admin': True, 'primary_title': 'Companies'
+        'companies': page_obj, 'is_admin': True, 'primary_title': 'Companies'
     })
 
 def handle_user_view(request):
@@ -34,29 +28,18 @@ def handle_user_view(request):
         return redirect('display_company_info', company_id=member_company.company_id)
     return render(request, 'no_company.html')
 
-
 @login_required
 def display_company_info(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    
-    if request.method == "POST":
-        page_number = request.POST.get('page-number', 1)
-        # Handle form submission (saving members) here
-        # After handling form submission, retain the current page number
-    else:
-        page_number = request.GET.get('page', 1)
-
-    # Get members of the company and paginate
+    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
     members = CompanyMembers.objects.filter(company_id=company_id).select_related('user')
     paginator = Paginator(members, 10)  # 10 members per page
-    paginated_members = paginator.get_page(page_number)
-
+    page_obj = paginator.get_page(page_number)
     is_admin = request.user.has_perm('admin')
-
     return render(request, 'company_info.html', {
         'primary_title': 'Company Info',
         'company': company,
-        'members': paginated_members,
+        'members': page_obj,
         'is_admin': is_admin
     })
 
@@ -74,14 +57,11 @@ def create_company(request):
             print("Form is not valid:", form.errors)
     else:
         form = CompanyForm()
-
     return render(request, 'company_info_create.html', {'form': form, 'primary_title': 'Create New Company'})
-
 
 @login_required
 def edit_company_info_post(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-
     if request.method == 'POST':
         form = CompanyForm(request.POST, instance=company)
         if form.is_valid():
@@ -89,7 +69,6 @@ def edit_company_info_post(request, company_id):
             return redirect('display_company_info', company_id=company.id)
     else:
         form = CompanyForm(instance=company)
-
     return render(request, 'company_edit.html', {
         'form': form,
         'company': company,
@@ -99,25 +78,12 @@ def edit_company_info_post(request, company_id):
 @login_required
 def display_company_members(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    
-    if request.method == "POST":
-        page_number = request.POST.get('page-number', 1)
-        # Handle form submission (saving members) here
-        # After handling form submission, retain the current page number
-    else:
-        page_number = request.GET.get('page', 1)
-    
-    # Retrieve members of the company
+    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
     members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
-    
-    # Filter User queryset to only include members of the company
     users = User.objects.filter(id__in=members_ids)
-    
     paginator = Paginator(users, 10)  # 10 users per page
     page_obj = paginator.get_page(page_number)
-
     members_ids_list = list(members_ids)
-
     return render(request, 'display_members.html', {
         'users': page_obj.object_list,
         'company': company,
@@ -128,24 +94,14 @@ def display_company_members(request, company_id):
 @login_required
 def edit_company_members(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    
-    if request.method == "POST":
-        page_number = request.POST.get('page-number', 1)
-        # Handle form submission (saving members) here
-        # After handling form submission, retain the current page number
-    else:
-        page_number = request.GET.get('page', 1)
-    
+    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
     paginator = Paginator(User.objects.all(), 10)  # 10 users per page
     page_obj = paginator.get_page(page_number)
-
     members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
     members_ids_list = list(members_ids)
-
     return render(request, 'edit_members.html', {
         'users': page_obj.object_list,
         'company': company,
-        'paginator': paginator,
         'page_obj': page_obj,
         'members_ids_list': members_ids_list
     })
@@ -156,28 +112,12 @@ def edit_company_members_post(request, company_id):
     if request.method == 'POST':
         company = get_object_or_404(Company, id=company_id)
         checkbox_values = request.POST.getlist('member-checkbox')
-        page_num = request.POST.get('page-number')
-        users_for_delete = Paginator(User.objects.all(), 9).page(int(page_num))
+        page_number = request.POST.get('page-number')
+        users_for_delete = Paginator(User.objects.all(), 10).page(int(page_number))
         members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
         members_ids_list = list(members_ids)
-
-        # Clear members so only those with checkboxes are left in DB.
         CompanyMembers.objects.filter(user_id__in=users_for_delete).delete()
-
         for value in checkbox_values:
             user = get_object_or_404(User, id=value)
             new_member = CompanyMembers.objects.create(company_id=company.id, user_id=user.id)
-
         return redirect('display_company_members', company_id=company.id)
-
-
-# companies list view
-
-# company view
-
-# company edit view
-
-# company members view
-
-
-# company members edit view
