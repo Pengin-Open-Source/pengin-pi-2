3fa7ed86aeabbfeb6d69a43f458790fda2429f9c
franfif <francis.barrow@gmail.com>
2024-07-23 16:59:12 -0400

    Add contract model

--- contracts/models.py
+++ contracts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Contract(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT, null=False)
+    type = models.CharField(max_length=100)
+    content = models.TextField()
+    service_date = models.DateTimeField(null=True)
+    expiration_date = models.DateTimeField(null=True)
+
+    def __str__(self):
+        return f"Contract {self.type} for {self.customer.name}"
