e46c0f93153b21b3c724ae773d6560c1917d5d41
franfif <francis.barrow@gmail.com>
2024-07-29 11:07:39 -0400

    Import Customer model in contracts app models

--- contracts/models.py
+++ contracts/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 import uuid
 
+from orders.models import Customer
+
 
 class Contract(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
-    customer = models.ForeignKey('Customer', on_delete=models.PROTECT, null=False)
+    customer = models.ForeignKey(Customer, on_delete=models.PROTECT, null=False)
     type = models.CharField(max_length=100)
     content = models.TextField()
     service_date = models.DateTimeField(null=True)
