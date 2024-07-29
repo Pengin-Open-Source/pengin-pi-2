37d0325410cd28126ca23fd51faa40d0addb0f99
franfif <francis.barrow@gmail.com>
2024-07-29 12:53:28 -0400

    Add date widget is contract form

--- contracts/forms.py
+++ contracts/forms.py
@@ -5,6 +5,9 @@ from .models import Contract
 
 class ContractForm(forms.ModelForm):
 
+    service_date = forms.DateField(widget=forms.SelectDateWidget)
+    expiration_date = forms.DateField(widget=forms.SelectDateWidget)
+
     class Meta:
         model = Contract
         fields = ['customer', 'type', 'content', 'service_date', 'expiration_date',]
