9ddc11b8cd6f9592af0bc7740c63c0e259624f20
mvetom <onelchela@live.com>
2024-08-08 09:27:36 -0400

    Change auth_tools groups_required method to allow any admin to bypass the decorator's "gatekeeping "  based on group membership.  Block Unvalidated Admin Staff members from seeing Admin-only menu links.

--- util/security/auth_tools.py
+++ util/security/auth_tools.py
@@ -6,7 +6,8 @@ def group_required(group_name):
     def decorator(view_func):
         @wraps(view_func)
         def wrapped_view(request, *args, **kwargs):
-            if request.user.is_authenticated and request.user.groups.filter(name=group_name).exists():
+            # There is no group staff cannot access
+            if request.user.is_authenticated and (request.user.is_staff or request.user.groups.filter(name=group_name).exists()):
                 return view_func(request, *args, **kwargs)
             else:
                 return HttpResponseForbidden("<h1> You don't have permission to access this page. </h1>")
