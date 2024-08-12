344a7a9b774a7ca3dcacd8c47eadac4753af1be3
franfif <francis.barrow@gmail.com>
2024-08-12 17:01:25 -0400

    Add event create and detail url patterns

--- events/urls.py
+++ events/urls.py
@@ -6,5 +6,7 @@ app_name = 'calendar'
 
 urlpatterns = [
     path('', CalendarMonth.as_view(), name='calendar'),
-    path('<int:year>/<int:month>/', CalendarMonth.as_view(), name='calendar_month'),
+    path('<int:year>/<int:month>/', CalendarMonth.as_view(), name='calendar-month'),
+    path('create_event/', CreateEvent.as_view(), name='create-event'),
+    path('<uuid:event_id>/', DetailEvent.as_view(), name='detail-event'),
 ]
