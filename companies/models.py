afacc7f99f496d9bed6613780bf515778d1ceacd
mvetom <onelchela@live.com>
2024-09-23 10:40:04 -0400

    Add dropdown option to create a new company on the companies list page.  Check for user  "is_staff"  when checking  if a user has admin privileges. (Now admin users are correctly redirected to the the list of all companies when they click on the Company link).  Make Address2 optional when creating new companies

--- companies/models.py
+++ companies/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from main.models.users import User  
+from main.models.users import User
 import uuid
 
 
@@ -13,7 +13,7 @@ class Company(models.Model):
     zipcode = models.CharField(max_length=50)
     email = models.EmailField(max_length=100, unique=True)
     address1 = models.CharField(max_length=50)
-    address2 = models.CharField(max_length=50)
+    address2 = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -21,9 +21,10 @@ class Company(models.Model):
 
 class CompanyMembers(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='members', null=True, blank=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)  # Allow null for user
-
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name='members', null=True, blank=True)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, null=True)  # Allow null for user
 
     def __str__(self):
         return str(self.id)
--- companies/views.py
+++ companies/views.py
@@ -2,19 +2,22 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
 from .models import Company, CompanyMembers
-from main.models.users import User  
+from main.models.users import User
 from .forms import CompanyForm
 from django_ratelimit.decorators import ratelimit
 
+
 @login_required
 def display_companies_home(request):
-    if request.user.has_perm('admin_permission'):
+    if request.user.is_staff:
         return handle_admin_view(request)
     else:
         return handle_user_view(request)
 
+
 def handle_admin_view(request):
-    page_number = request.POST.get('page_number', 1) if request.method == "POST" else request.GET.get('page', 1)
+    page_number = request.POST.get(
+        'page_number', 1) if request.method == "POST" else request.GET.get('page', 1)
     companies = Company.objects.all()
     paginator = Paginator(companies, 10)  # 10 companies per page
     page_obj = paginator.get_page(page_number)
@@ -22,26 +25,32 @@ def handle_admin_view(request):
         'companies': page_obj, 'is_admin': True, 'primary_title': 'Companies'
     })
 
+
 def handle_user_view(request):
-    member_company = CompanyMembers.objects.filter(user_id=request.user.id).first()
+    member_company = CompanyMembers.objects.filter(
+        user_id=request.user.id).first()
     if member_company:
         return redirect('display_company_info', company_id=member_company.company_id)
     return render(request, 'no_company.html')
 
+
 @login_required
 def display_company_info(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
-    members = CompanyMembers.objects.filter(company_id=company_id).select_related('user')
+    page_number = request.POST.get(
+        'page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
+    members = CompanyMembers.objects.filter(
+        company_id=company_id).select_related('user')
     paginator = Paginator(members, 10)  # 10 members per page
     page_obj = paginator.get_page(page_number)
-    is_admin = request.user.has_perm('admin')
+    is_admin = request.user.is_staff
     return render(request, 'company_info.html', {
         'primary_title': 'Company Info',
         'company': company,
         'members': page_obj,
         'is_admin': is_admin
     })
+
 
 @login_required
 def create_company(request):
@@ -59,6 +68,7 @@ def create_company(request):
         form = CompanyForm()
     return render(request, 'company_info_create.html', {'form': form, 'primary_title': 'Create New Company'})
 
+
 @login_required
 def edit_company_info_post(request, company_id):
     company = get_object_or_404(Company, id=company_id)
@@ -75,11 +85,14 @@ def edit_company_info_post(request, company_id):
         'primary_title': 'Edit Company'
     })
 
+
 @login_required
 def display_company_members(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
-    members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
+    page_number = request.POST.get(
+        'page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
+    members_ids = CompanyMembers.objects.filter(
+        company_id=company.id).values_list('user_id', flat=True)
     users = User.objects.filter(id__in=members_ids)
     paginator = Paginator(users, 10)  # 10 users per page
     page_obj = paginator.get_page(page_number)
@@ -91,13 +104,16 @@ def display_company_members(request, company_id):
         'members_ids_list': members_ids_list
     })
 
+
 @login_required
 def edit_company_members(request, company_id):
     company = get_object_or_404(Company, id=company_id)
-    page_number = request.POST.get('page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
+    page_number = request.POST.get(
+        'page-number', 1) if request.method == "POST" else request.GET.get('page', 1)
     paginator = Paginator(User.objects.all(), 10)  # 10 users per page
     page_obj = paginator.get_page(page_number)
-    members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
+    members_ids = CompanyMembers.objects.filter(
+        company_id=company.id).values_list('user_id', flat=True)
     members_ids_list = list(members_ids)
     return render(request, 'edit_members.html', {
         'users': page_obj.object_list,
@@ -106,18 +122,22 @@ def edit_company_members(request, company_id):
         'members_ids_list': members_ids_list
     })
 
-@ratelimit(key='ip', rate='10/m', method='POST', block=True)  
+
+@ratelimit(key='ip', rate='10/m', method='POST', block=True)
 @login_required
 def edit_company_members_post(request, company_id):
     if request.method == 'POST':
         company = get_object_or_404(Company, id=company_id)
         checkbox_values = request.POST.getlist('member-checkbox')
         page_number = request.POST.get('page-number')
-        users_for_delete = Paginator(User.objects.all(), 10).page(int(page_number))
-        members_ids = CompanyMembers.objects.filter(company_id=company.id).values_list('user_id', flat=True)
+        users_for_delete = Paginator(
+            User.objects.all(), 10).page(int(page_number))
+        members_ids = CompanyMembers.objects.filter(
+            company_id=company.id).values_list('user_id', flat=True)
         members_ids_list = list(members_ids)
         CompanyMembers.objects.filter(user_id__in=users_for_delete).delete()
         for value in checkbox_values:
             user = get_object_or_404(User, id=value)
-            new_member = CompanyMembers.objects.create(company_id=company.id, user_id=user.id)
+            new_member = CompanyMembers.objects.create(
+                company_id=company.id, user_id=user.id)
         return redirect('display_company_members', company_id=company.id)
