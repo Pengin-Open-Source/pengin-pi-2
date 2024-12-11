7663992be745b5ee9494b772b992b8d84015b8a0
mvetom <onelchela@live.com>
2024-12-11 12:27:33 -0500

    Make the Edit and Create Event forms display user names instead of emails

--- events/forms.py
+++ events/forms.py
@@ -1,8 +1,25 @@
 from django import forms
+from main.models import User
 from .models import Event
 
 
+# Credit to https://stackoverflow.com/questions/49114304/to-field-name-argument-on-a-modelchoicefield-doesnt-seem-to-be-working
+# -> Alasdair's answer, Plus help from Google Gemini on how to get the selection box to display User names instead of emails without
+# changing the User __str__ method.
+class UserModelMultipleChoiceField(forms.ModelMultipleChoiceField):
+    def label_from_instance(self, obj):
+        return obj.name
+
+
+class UserModelChoiceField(forms.ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.name
+
+
 class EventForm(forms.ModelForm):
+    participants = UserModelMultipleChoiceField(queryset=User.objects.all())
+    organizer = UserModelChoiceField(queryset=User.objects.all())
+
     class Meta:
         model = Event
         fields = [
@@ -15,7 +32,8 @@ class EventForm(forms.ModelForm):
             "participants",
             "roles",
         ]
+
         widgets = {
             "start_datetime": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "end_datetime": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+            "end_datetime": forms.DateTimeInput(attrs={"type": "datetime-local"})
         }
