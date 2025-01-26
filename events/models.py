d600e727a8b1c28be5f9702b94ee204ae71045b0
mvetom <onelchela@live.com>
2025-01-26 10:38:32 -0500

    Add in Event History

--- events/models.py
+++ events/models.py
@@ -38,6 +38,79 @@ class Event(models.Model):
     def end_time(self):
         return self.end_datetime.time().strftime("%I:%M %p")
 
+    def save(self, *args, **kwargs):
+        save_method = self.row_action
+        # all backups must complete properly for changes to be saved
+        with transaction.atomic():
+            # Do backup of current values in the row first.
+            # (Note we backup before a DELETE.  Frequently,  a
+            # row will have no backup history until we enter DELETE)
+            # Rows will still be backed up even if 'ERROR' was assigned to the row_action.
+            if save_method != "CREATE":
+                original_event = Event.objects.get(pk=self.pk)
+
+                # KEY ASSUMPTION FOR THE FOLLOWING TO BE ACCURATE:
+                # No one will change the groups-Event relationship, except by editing the Event
+                # (IE,  adding or removing a Role from An Event, on An Event "Edit" page)
+                # Also note that while the pk of group associated with an Event should not change,
+                # it is theorectically possible for the NAME of group to change since the time it was
+                # Linked to this Event (hence we save both in history)
+                # NOTE THAT I equivocate between roles and groups and this time.
+                event_roles = original_event.groups.all()
+                groups_snapshot = list(event_roles.values('pk', 'name'))
+                if original_event.last_edited_by:
+                    event_editor = original_event.last_edited_by.pk
+                else:
+                    event_editor = None
+                event_backup = EventHistory(event_id=original_event.pk, title=original_event.title, description=original_event.description,
+                                            location=original_event.location, date=original_event.date,
+                                            start_datetime=original_event.start_datetime, end_datetime=original_event.end_datetime,
+                                            author=original_event.author.pk, organizer=original_event.author.pk, last_edited_by=event_editor,
+                                            roles=groups_snapshot, row_action=original_event.row_action)
+                event_backup.save()
+
+            # else: this is a newly created Event; don't save it to backup table yet
+
+            # In any event (but a rollback),  save this new Event or Event update to the database.
+            super().save(*args, **kwargs)
+
+            # if this is a pre-delete save,  the Event row will have been updated to contain
+            # 1) The action/method: "DELETE"
+            # 2) The User who did the Delete
+            # 3) The time of the deletion
+            # We need to make sure this information is copied into Event history
+            # before we delete this Event.
+            # (If Event history needs to be totally deleted, that should be done
+            # by a DBA)
+            if save_method == 'DELETE':
+                # FYI, reusing groups_snapshot since it shouldn't have changed since the "last change" backup save
+                archived_event = EventHistory(event_id=self.pk, title=self.title, description=self.description,
+                                              location=self.location, date=self.date,
+                                              start_datetime=self.start_datetime, end_datetime=self.end_datetime,
+                                              author=self.author.pk, organizer=self.author.pk, last_edited_by=event_editor,
+                                              roles=groups_snapshot, row_action=self.row_action)
+                archived_event.save()
+
+
+class EventHistory(models.Model):
+    id = models.UUIDField(
+        primary_key=True, default=uuid.uuid4, editable=False)
+    event_id = models.UUIDField(db_index=True)
+    date = models.DateTimeField()
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+
+    # currently not allowing these two fields to be null,  since the Event table has these in on_delete PROTECT mode
+    author = models.UUIDField(db_index=True)
+    organizer = models.UUIDField(db_index=True)
+
+    last_edited_by = models.UUIDField(db_index=True,  null=True)
+    roles = models.JSONField()
+    row_action = models.CharField(max_length=10, default='ERROR')
+
 
 class EventParticipant(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
