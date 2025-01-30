65773eb3ef9541f4ff9888d6abef12f62527cb22
mvetom <onelchela@live.com>
2025-01-30 10:07:26 -0500

    Get currently selected participants to be selected in Edit page.

--- events/forms.py
+++ events/forms.py
@@ -11,6 +11,9 @@ class UserModelMultipleChoiceField(forms.ModelMultipleChoiceField):
     def label_from_instance(self, obj):
         return obj.name
 
+    def __init__(self, queryset, **kwargs):
+        super().__init__(queryset, **kwargs)
+
 
 class UserModelChoiceField(forms.ModelChoiceField):
     def label_from_instance(self, obj):
@@ -35,10 +38,19 @@ class EventForm(forms.ModelForm):
             "roles",
         ]
 
-        widgets = {
-            "start_datetime": DateTimePickerInput(),
-            "end_datetime": DateTimePickerInput(),
-        }
+    # I was having trouble getting the currently selected participants to load in the Edit view
+    # Gemini suggested setting the initial participants field within the init function.
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and not self.instance._state.adding:  #
+            current_participants_ids = EventParticipant.objects.filter(
+                event_id=self.instance.id).values_list('participant_id', flat=True)
+            self.fields['participants'].initial = current_participants_ids
+
+    widgets = {
+        "start_datetime": DateTimePickerInput(),
+        "end_datetime": DateTimePickerInput(),
+    }
 
     def clean(self):
         cleaned_up_data = super().clean()
