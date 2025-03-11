b9cf33a34e572cc41ae0431332ca24df3cba034d
mvetom <onelchela@live.com>
2025-03-11 13:01:55 -0400

    User should be able to see an event or duplicate it if it a participant in it

--- events/permissions.py
+++ events/permissions.py
@@ -10,9 +10,12 @@ def can_create_or_see_event(request, event_id=None):
     if event_id is None:
         return True
     # If event exists, user has permission if they are the author, organizer, or a participant - or staff
+    # Participants have permission to a) See Events they are in  b) duplicate events they are in
     event = get_object_or_404(Event, id=event_id)
-    return (request.user.is_staff or request.user in [event.author, event.organizer]
-            or request.user in event.participants.all())
+    participant_ids = event.participants.values_list(
+        'participant_id', flat=True)
+
+    return (request.user.is_staff or request.user.id in participant_ids or request.user in [event.author,  event.organizer])
 
 
 def can_change_event(request, event_id):
