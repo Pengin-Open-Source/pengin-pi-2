7e019bc7d587df7c216a02a68008fe68b476630a
mvetom <onelchela@live.com>
2025-03-18 13:14:46 -0400

    Update Subgroups,  add in GroupSpecialAccess tables,  these are for granting access from one group/role to another.  Provide edit access in the admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,9 @@
 # admin.py
 from django.contrib import admin
 
-from .models import User, Address
+from .models import User, Address, SubGroup, GroupSpecialAccess
 
 admin.site.register(User)
 admin.site.register(Address)
+admin.site.register(SubGroup)
+admin.site.register(GroupSpecialAccess)
