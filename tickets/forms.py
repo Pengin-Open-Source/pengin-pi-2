a3fe8d8471c32f3432804e760e68f85de7dfc011
mvetom <onelchela@live.com>
2026-01-18 10:34:07 -0500

    fix(tickets): fixed bug where Ticket Reopen Request Reviewer wasn't showing up on past handled requests detail page

--- tickets/forms.py
+++ tickets/forms.py
@@ -370,9 +370,9 @@ class PastTicketOpenRequestForm(forms.ModelForm):
         self.fields['author'].initial = User.objects.filter(
             id=self.instance.author.id)
         self.fields['reviewer'].queryset = User.objects.filter(
-            id=self.instance.author.id)
+            id=self.instance.reviewer.id)
         self.fields['reviewer'].initial = User.objects.filter(
-            id=self.instance.author.id)
+            id=self.instance.reviewer.id)
         if self.instance.bypass_initiated_by:
             self.fields['bypass_initiated_by'].queryset = User.objects.filter(
                 id=self.instance.bypass_initiated_by.id)
