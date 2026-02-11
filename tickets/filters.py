2494407025082f2cc12003ff85c103ad2a3fe1ca
mvetom <onelchela@live.com>
2026-02-11 13:11:57 -0500

    feature(tickets): Get Tom Select working with one of the search fields.

--- tickets/filters.py
+++ tickets/filters.py
@@ -17,7 +17,7 @@ class TicketFilter(django_filters.FilterSet):
     summary = django_filters.ChoiceFilter(
         lookup_expr='icontains', label='Search Ticket Summaries....',
         choices=[],
-        widget=forms.TextInput(
+        widget=forms.Select(
             attrs={'class': 'tom-select-enabled'})
     )
 
@@ -27,6 +27,15 @@ class TicketFilter(django_filters.FilterSet):
     role = django_filters.CharFilter(field_name='role__name',  # Actually searches the Group table's name field
                                      lookup_expr='icontains', label='Search Ticket by Role....',
                                      widget=forms.TextInput(attrs={'list': 'role-options', 'class': 'form-control'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        visible_tickets = self.queryset
+        title_options = visible_tickets.values_list(
+            'summary', flat=True).distinct()
+        self.filters['summary'].extra['choices'] = [
+            (t, t) for t in title_options]
+        # self.filters['category'].extra['queryset'] = self.queryset
 
     class Meta:
         model = Ticket
