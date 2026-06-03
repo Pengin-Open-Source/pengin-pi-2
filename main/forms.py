ee0a352ed53e1c8e48b7649e2dad6229ae7ad5d7
mvetom <onelchela@live.com>
2026-06-03 17:03:36 -0400

    chore(admin):   More clear warnings and labels re: allowed combinations of the is_active, is_staff, and validated attributes.

--- main/forms.py
+++ main/forms.py
@@ -52,10 +52,18 @@ class GroupManagerDjangoSiteFormAdmin(admin.ModelAdmin):
 # of a User if the SuperUser inactivates it.
 # They way the SuperUser doesn't have to know/remember
 # to uncheck two boxes
+
+
 class UserAdminForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
+
+        labels = {
+            'is_active': 'User is Active?  (If Unchecked, "Validated" will be auto-unchecked upon Save)',
+            'is_staff': 'Staff/Admin Level Access? (Cannot be checked if User is not Active & Validated)',
+            # You can even use help_text to be even more descriptive
+        }
 
     def clean(self):
         cleaned_data = super().clean()
@@ -63,13 +71,21 @@ class UserAdminForm(forms.ModelForm):
         # Get the current form states for both fields
         is_active = cleaned_data.get('is_active')
 
-        # NOTE: Swap 'is_validated' with the exact name of your validation field
         is_validated = cleaned_data.get('validated')
+
+        is_staff = cleaned_data.get('is_staff')
 
         # The Pre-Database Kill Switch:
         # If the user is being deactivated, automatically turn off the validation flag.
         if is_active is False and is_validated is True:
             cleaned_data['validated'] = False
+
+        if is_staff and (not is_validated or not is_active):
+            raise forms.ValidationError({
+                "Cannot invalidate or inactivate Staff.  Remove Staff status first"
+            })
+
+        # if this is a staff user,  don't allow
 
         return cleaned_data
 
